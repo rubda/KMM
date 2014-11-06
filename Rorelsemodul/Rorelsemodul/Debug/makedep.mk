61711ebec71c976697d7fd10c1539fa9a02a88a5
unknown <Student@TFK-0074.ad.liu.se>
2014-11-06 17:07:43 +0100

    Started to fix UART between modules

--- Rorelsemodul/Rorelsemodul/Debug/makedep.mk
+++ Rorelsemodul/Rorelsemodul/Debug/makedep.mk
@@ -2,7 +2,11 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+atmega-timers.c
+
 Rorelsemodul.c
 
 servo_uart.c
+
+UART.c
 
