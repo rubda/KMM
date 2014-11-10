0ac85e1177b284b4b03640187a8035b18a5382d1
Jakub007 <biniedajakub07@gmail.com>
2014-11-10 13:49:28 +0100

    uart send string

---
+++ Sensormodul/Sensormodul/UART.c
@@ -0,0 +1,113 @@
+#include "UART.h"
+
+char buffer[256];
+uint8_t buffer_size = 0;
+
+int uart_read_string(char *s, int size);
+
+void uart_init(long baud)
+{
+	// Initialize serial
+	UBRR0 = ((F_CPU / 16 + baud / 2) / baud - 1);         /* (16 MHz / 16 / 38400) - 1 */
+	set_bit(UCSR0B, RXEN0);                               /* enable RX */
+	set_bit(UCSR0B, TXEN0);                               /* enable TX */
+
+	set_bit(UCSR0B, RXCIE0);
+	UCSR0C = (3 << UCSZ00);
+}
+
+void check_rx() {
+	if (bit_is_set(UCSR0A, RXC0)){
+		buffer_size = uart_read_string(buffer, 255);
+	}
+}
+
+ISR(USART0_RX_vect){
+	cli();
+	check_rx();
+	sei();
+}
+
+char uart_read_char()
+{
+	while(!bit_is_set(UCSR0A, RXC0));
+	return UDR0;
+}
+
+void uart_send_char(uint8_t data)
+{
+	while(!bit_is_set(UCSR0A, UDRE0));
+	UDR0 = data;
+}
+
+int uart_read_string(char *s, int size)
+{
+	uint8_t i = 0;
+	char c;
+	
+	while(i < size - 1){
+		c = uart_read_char();
+		if(c == '\0')
+			break;
+		else if(c == '#'){
+			i = 0;
+		}
+		s[i++] = c;
+	}
+	
+	s[i] = 0;
+	
+	return i + 1;
+}
+
+void uart_send_string(char *s, uint8_t size)
+{
+	uint8_t i = 0;
+	
+	uint8_t checksum = 0;
+	
+	for(i = 0; i < size; ++i){
+		if(i >= 2) checksum += s[i];
+		uart_send_char(s[i]);
+	}
+	uart_send_char(~checksum);
+	while(!bit_is_set(UCSR0A, TXC0));
+}
+
+uint8_t got_message(){
+	return buffer_size;
+}
+
+attribute create_attribute(char *string, int start, int end){
+	char str[end-start];
+	int i;
+	for(i = start; i < end; ++i){
+		str[i-start] = string[i];
+	}
+	
+	attribute attr;
+	attr.data = str;
+	attr.length = end-start-1;
+	
+	return attr;
+}
+
+// DISABLE INTERRUPT?!
+uart_message get_message(){
+	uart_message message;
+	message.length = 0;
+	if(got_message() && buffer[0] == '#'){
+		//#COMMAND:ATTR1:ATTRN;	
+		int i;
+		int start = 1;
+		for(i = start; i < buffer_size && buffer[i-1] != ';'; ++i){
+			if(buffer[i] == ':' || buffer[i] == ';'){
+				message.data[message.length++] = create_attribute(buffer, start, i);
+				start = i+1;
+			}
+		}
+		buffer_size = 0;
+		return message;
+	}
+	return message;
+}
---
+++ Sensormodul/Sensormodul/UART.h
@@ -0,0 +1,45 @@
+#define F_CPU 16000000UL
+
+#define BV(bit)               (1 << bit)
+#define set_bit(byte, bit)    (byte |= BV(bit))  // old sbi()
+#define clear_bit(byte, bit)  (byte &= ~BV(bit)) // old cbi()
+#define toggle_bit(byte, bit) (byte ^= BV(bit))
+
+#define PRESCALER 1 //Beroende på vad som passar för att timea RX
+#define TICKS 1		//Beroende på vad som passar för att timea RX
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+//#include "atmega-timers.h"
+
+/************************************************************************/
+/* Baudrate: 19200                                                      */
+/************************************************************************/
+#define UART_SETTINGS 0x0001
+
+#define UART_RX PORTD2
+#define UART_TX PORTD3
+
+struct attribute 
+{
+	char * data;
+	uint8_t length;
+};
+
+typedef struct attribute attribute;
+
+struct uart_message
+{
+	attribute data[10];
+	uint8_t length;
+};
+typedef struct uart_message uart_message;
+
+void uart_init(long baud);
+
+uint8_t got_message();
+uart_message get_message();
+//uart_message create_message(char name[], char attributes[][]);
+void send_message(uart_message m);
+void uart_send_string(char *s, uint8_t size);
