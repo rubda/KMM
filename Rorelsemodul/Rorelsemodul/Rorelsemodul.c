c53a0d2cd40e0731222679886d8879122c58f0f2
Calle Arvidsson <calle.arvidssons@gmail.com>
2014-11-12 15:48:13 +0100

    rorelsemodul - big update

--- Rorelsemodul/Rorelsemodul/Rorelsemodul.c
+++ Rorelsemodul/Rorelsemodul/Rorelsemodul.c
@@ -9,39 +9,28 @@
 
 #include "servo_uart.h"
 #include <avr/interrupt.h>
-#include "UART.h"
-//#include <util/delay.h>
-
-//volatile unsigned char data;
 
 int main(void)
 {	
 	sei();
-	uart_init(1200);
+	suart_init(1000000);
+		
+	uint8_t test1[] = {0xFF, 0x00};
+		
+	_delay_ms(1000);
+	set_servo_speed();
+	robot_start_position();
+	//_delay_ms(2000);
+	_delay_ms(1000);
 	
-	//www.wormfood.net/avrbaudcalc.php
-	//extremeelectronics.co.in/avr-tutorials/using-the-usart-of-avr-microcontrollers/
-	//UBRR = 23, 0x0017
-	
-	DDRB = 0xFF;
 	
 	while(1)
 	{
-		uart_send_char('0');	
-		_delay_us(10);
-    }
-}
-
-ISR(USART0_RX_vect){
-	//while (!bit_is_set(UCSR0A, RXC0));
-	//toggle_bit(PORTB, PORTB1);
-	//if(UDR0 == '0')
-	//	set_bit(PORTB, PORTB0);
-	//else
-	//	clear_bit(PORTB, PORTB0);
-	PORTB = UDR0;
+		
+	}
 }
 
 ISR(USART1_TX_vect){
 	SUART_RX_ACTIVE;
 }
+
