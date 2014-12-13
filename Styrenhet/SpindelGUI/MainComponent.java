3688440bf22a7db06c8925842afa3457e0e48f4d
Jonas Andersson <jcm.andersson@gmail.com>
2014-12-13 15:31:16 +0100

    Forgot to readd som comments

--- Styrenhet/SpindelGUI/MainComponent.java
+++ Styrenhet/SpindelGUI/MainComponent.java
@@ -12,162 +12,162 @@ public class MainComponent extends JComponent {
     Boolean rr, rl, f, b;
 
     public MainComponent(){
-        rr = false;
-        rl = false;
-        f = false;
-        b = false;
-        addBindings();
-        setFocusable(true);
+	rr = false;
+	rl = false;
+	f = false;
+	b = false;
+	addBindings();
+	setFocusable(true);
 
 	setupGamepad();
     }
 
     private void addBindings(){
 
-        InputMap map = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+	InputMap map = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
 
 
-        map.put(KeyStroke.getKeyStroke("pressed LEFT"), "rotateLeft");
-        map.put(KeyStroke.getKeyStroke("pressed RIGHT"), "rotateRight");
-        map.put(KeyStroke.getKeyStroke("pressed UP"), "walkForward");
-        map.put(KeyStroke.getKeyStroke("pressed DOWN"), "walkBackwards");
-        map.put(KeyStroke.getKeyStroke("pressed 7"), "strafeLeftForward");
-        map.put(KeyStroke.getKeyStroke("pressed 9"), "strafeRightForward");
-        map.put(KeyStroke.getKeyStroke("pressed R"), "reset");
+	map.put(KeyStroke.getKeyStroke("pressed LEFT"), "rotateLeft");
+	map.put(KeyStroke.getKeyStroke("pressed RIGHT"), "rotateRight");
+	map.put(KeyStroke.getKeyStroke("pressed UP"), "walkForward");
+	map.put(KeyStroke.getKeyStroke("pressed DOWN"), "walkBackwards");
+	map.put(KeyStroke.getKeyStroke("pressed 7"), "strafeLeftForward");
+	map.put(KeyStroke.getKeyStroke("pressed 9"), "strafeRightForward");
+	map.put(KeyStroke.getKeyStroke("pressed R"), "reset");
 
-        map.put(KeyStroke.getKeyStroke("released 7"), "stopStrafeLeftForward");
-        map.put(KeyStroke.getKeyStroke("released 9"), "stopStrafeRightForward");
-        map.put(KeyStroke.getKeyStroke("released LEFT"), "stopRotateLeft");
-        map.put(KeyStroke.getKeyStroke("released RIGHT"), "stopRotateRight");
-        map.put(KeyStroke.getKeyStroke("released UP"), "stopWalkingForward");
-        map.put(KeyStroke.getKeyStroke("released DOWN"), "stopWalkingBackwards");
+	map.put(KeyStroke.getKeyStroke("released 7"), "stopStrafeLeftForward");
+	map.put(KeyStroke.getKeyStroke("released 9"), "stopStrafeRightForward");
+	map.put(KeyStroke.getKeyStroke("released LEFT"), "stopRotateLeft");
+	map.put(KeyStroke.getKeyStroke("released RIGHT"), "stopRotateRight");
+	map.put(KeyStroke.getKeyStroke("released UP"), "stopWalkingForward");
+	map.put(KeyStroke.getKeyStroke("released DOWN"), "stopWalkingBackwards");
 
 
-        Action reset = new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println("reset");
-                requestFocus();
-            }
-        };
+	Action reset = new AbstractAction() {
+	    @Override
+	    public void actionPerformed(ActionEvent actionEvent) {
+		System.out.println("reset");
+		requestFocus();
+	    }
+	};
 
-        Action rotateLeft = new AbstractAction(){
-            public void actionPerformed(final ActionEvent e) {
-                rl = true;
-                if(f && !rr && !b){
-                    Communication.sendAction("#turn:l;");
-                }
-                else{
-                    Communication.sendAction("#rotate:l;");
-                }
-            }
-        };
-        Action rotateRight = new AbstractAction(){
-            public void actionPerformed(final ActionEvent e) {
-                rr = true;
-                if(f && !rl && !b){
-                    Communication.sendAction("#turn:r;");
-                }
-                else{
-                Communication.sendAction("#rotate:r;");
-                }
-            }
-        };
-        Action walkForward = new AbstractAction(){
-            public void actionPerformed(final ActionEvent e) {
-                f = true;
-                if(rl && !rr && !b){
-                Communication.sendAction("#turn:l;");
-                }
-                else if(!rl && rr && !b){
-                    Communication.sendAction("#turn:r;");
-                }
-                else{
-                    Communication.sendAction("#walk:f:0;");
+	Action rotateLeft = new AbstractAction(){
+	    public void actionPerformed(final ActionEvent e) {
+		rl = true;
+		if(f && !rr && !b){
+		    Communication.sendAction("#turn:l;");
+		}
+		else{
+		    Communication.sendAction("#rotate:l;");
+		}
+	    }
+	};
+	Action rotateRight = new AbstractAction(){
+	    public void actionPerformed(final ActionEvent e) {
+		rr = true;
+		if(f && !rl && !b){
+		    Communication.sendAction("#turn:r;");
+		}
+		else{
+		    Communication.sendAction("#rotate:r;");
+		}
+	    }
+	};
+	Action walkForward = new AbstractAction(){
+	    public void actionPerformed(final ActionEvent e) {
+		f = true;
+		if(rl && !rr && !b){
+		    Communication.sendAction("#turn:l;");
+		}
+		else if(!rl && rr && !b){
+		    Communication.sendAction("#turn:r;");
+		}
+		else{
+		    Communication.sendAction("#walk:f:0;");
 
-                }
-            }
-        };
+		}
+	    }
+	};
 
-        Action strafeLeftForward = new AbstractAction() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                Communication.sendAction("#walk:f:45;");
-            }
-        };
-        Action strafeRightForward = new AbstractAction() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                Communication.sendAction("#walk:f:315;");
-            }
-        };
-        Action walkBackwards = new AbstractAction(){
-            public void actionPerformed(final ActionEvent e) {
-                b = true;
-                Communication.sendAction("#walk:b;");
-            }
-        };
-        Action stopRotateLeft = new AbstractAction()
-        {
-            public void actionPerformed(final ActionEvent e) {
-                rl = false;
-                if (!f && !b && !rr) {
-                    Communication.sendAction("#stop:after;");
-                }
-            }
-        };
-        Action stopRotateRight = new AbstractAction()
-        {
-            public void actionPerformed(final ActionEvent e) {
-                 rr = false;
-                if (!f && !b && !rl) {
-                    Communication.sendAction("#stop:after;");
-                }
-            }
-        };
-        Action stopWalkingForward = new AbstractAction()
-        {
-            public void actionPerformed(final ActionEvent e) {
-                f = false;
-                if(!b && !rl && !rr){
-                    Communication.sendAction("#stop:after;");
-                }
-            }
-        };
-        Action stopWalkingBackwards = new AbstractAction()
-        {
-            public void actionPerformed(final ActionEvent e) {
-                b = false;
-                if(!f && !rl && !rr){
-                    Communication.sendAction("#stop:after;");
-                }
-            }
-        };
-        Action stopStrafeLeftForward = new AbstractAction() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                Communication.sendAction("#stop:after;");
-            }
-        };
-        Action stopStrafeRightForward = new AbstractAction() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                Communication.sendAction("#stop:after;");
-            }
-        };
-        getActionMap().put("reset", reset);
-        getActionMap().put("rotateLeft", rotateLeft);
-        getActionMap().put("rotateRight", rotateRight);
-        getActionMap().put("walkForward", walkForward);
-        getActionMap().put("walkBackwards", walkBackwards);
-        getActionMap().put("strafeLeftForward", strafeLeftForward);
-        getActionMap().put("strafeRightForward", strafeRightForward);
-        getActionMap().put("stopRotateLeft", stopRotateLeft);
-        getActionMap().put("stopRotateRight", stopRotateRight);
-        getActionMap().put("stopWalkingForward", stopWalkingForward);
-        getActionMap().put("stopWalkingBackwards", stopWalkingBackwards);
-        getActionMap().put("stopStrafeLeftForward", stopStrafeLeftForward);
-        getActionMap().put("stopStrafeRightForward", stopStrafeRightForward);
+	Action strafeLeftForward = new AbstractAction() {
+	    @Override
+	    public void actionPerformed(final ActionEvent e) {
+		Communication.sendAction("#walk:f:45;");
+	    }
+	};
+	Action strafeRightForward = new AbstractAction() {
+	    @Override
+	    public void actionPerformed(final ActionEvent e) {
+		Communication.sendAction("#walk:f:315;");
+	    }
+	};
+	Action walkBackwards = new AbstractAction(){
+	    public void actionPerformed(final ActionEvent e) {
+		b = true;
+		Communication.sendAction("#walk:b;");
+	    }
+	};
+	Action stopRotateLeft = new AbstractAction()
+	{
+	    public void actionPerformed(final ActionEvent e) {
+		rl = false;
+		if (!f && !b && !rr) {
+		    Communication.sendAction("#stop:after;");
+		}
+	    }
+	};
+	Action stopRotateRight = new AbstractAction()
+	{
+	    public void actionPerformed(final ActionEvent e) {
+		rr = false;
+		if (!f && !b && !rl) {
+		    Communication.sendAction("#stop:after;");
+		}
+	    }
+	};
+	Action stopWalkingForward = new AbstractAction()
+	{
+	    public void actionPerformed(final ActionEvent e) {
+		f = false;
+		if(!b && !rl && !rr){
+		    Communication.sendAction("#stop:after;");
+		}
+	    }
+	};
+	Action stopWalkingBackwards = new AbstractAction()
+	{
+	    public void actionPerformed(final ActionEvent e) {
+		b = false;
+		if(!f && !rl && !rr){
+		    Communication.sendAction("#stop:after;");
+		}
+	    }
+	};
+	Action stopStrafeLeftForward = new AbstractAction() {
+	    @Override
+	    public void actionPerformed(final ActionEvent e) {
+		Communication.sendAction("#stop:after;");
+	    }
+	};
+	Action stopStrafeRightForward = new AbstractAction() {
+	    @Override
+	    public void actionPerformed(final ActionEvent e) {
+		Communication.sendAction("#stop:after;");
+	    }
+	};
+	getActionMap().put("reset", reset);
+	getActionMap().put("rotateLeft", rotateLeft);
+	getActionMap().put("rotateRight", rotateRight);
+	getActionMap().put("walkForward", walkForward);
+	getActionMap().put("walkBackwards", walkBackwards);
+	getActionMap().put("strafeLeftForward", strafeLeftForward);
+	getActionMap().put("strafeRightForward", strafeRightForward);
+	getActionMap().put("stopRotateLeft", stopRotateLeft);
+	getActionMap().put("stopRotateRight", stopRotateRight);
+	getActionMap().put("stopWalkingForward", stopWalkingForward);
+	getActionMap().put("stopWalkingBackwards", stopWalkingBackwards);
+	getActionMap().put("stopStrafeLeftForward", stopStrafeLeftForward);
+	getActionMap().put("stopStrafeRightForward", stopStrafeRightForward);
     }
 
 
@@ -191,6 +191,8 @@ public class MainComponent extends JComponent {
 	pollGamepad();
     }
 
+    boolean stopped = true;
+
     void pollGamepad() {
 	if(ps3 != null){
 	    ps3.poll();
@@ -206,23 +208,24 @@ public class MainComponent extends JComponent {
 	    if(x < -0.2 && Math.abs(x) > Math.abs(y)){
 		System.out.println("#rotate:l;");
 		Communication.sendAction("#rotate:l;");
+		stopped = false;
 	    }else if(x > 0.2 && Math.abs(x) > Math.abs(y)){
+		System.out.println("#rotate:r;");
 		Communication.sendAction("#rotate:r;");
+		stopped = false;
 	    }else if(Math.abs(rx) > 0.1 || Math.abs(ry) > 0.1){
-		    int angle = (int) Math.round(Math.toDegrees( Math.atan2(rx,-ry)));
-		    double ax = Math.abs(rx);
-		    double ay = Math.abs(ry);
-		    speed =  (int)(ax > ay ? 600*ax : 600*ay);
-		    System.out.println(angle);
-		    System.out.println(speed);
-		//Communication.sendAction("#walk:"+(angle)+";");
+		int angle = (int) Math.round(Math.toDegrees( Math.atan2(rx,-ry)));
+		double ax = Math.abs(rx);
+		double ay = Math.abs(ry);
+		speed =  (int)(ax > ay ? 600*ax : 600*ay);
+		Communication.sendAction("#walk:"+(angle)+";");
 		System.out.println("#walk:"+(angle)+";");
-
-
-	    }else{
-	    	//Communication.sendAction("#stop:0;");
+		stopped = false;
+	    }else if(!stopped){
+		Communication.sendAction("#stop:0;");
 		speed = 200;
 		System.out.println("#stop:0;");
+		stopped = true;
 	    }
 
 	    gamepadTimer.schedule(new java.util.TimerTask()
@@ -233,12 +236,14 @@ public class MainComponent extends JComponent {
 		}
 	    }, 100);
 
-	    try {
-		Thread.sleep(50);
-	    } catch (InterruptedException e) {
-		e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+	    if(!stopped){
+		try {
+		    Thread.sleep(50);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+		}
+		Communication.sendAction("#speed:"+speed+";");
 	    }
-	    Communication.sendAction("#speed:"+speed+";");
 	}
     }
 }
